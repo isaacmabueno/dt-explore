9099c48f9592847e7024925a17e658388c359f91
isaacmabueno <isaacmabueno@gmail.com>
2018-10-26 14:16:59 -0500

    creating content component and adding in bootstrap gridding

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import Nav from './Nav'
+import Nav from './Nav';
+import Content from './Content';
 import { Button } from 'react-bootstrap';
 
  class App extends Component {
    render() {
      return (
-       <div className="App">
-         <header className="App-header">
-           <Nav />
-         </header>
-       </div>
+         <section>
+            <div className="container-fluid">
+                <div className="row App">   
+                    
+                    <Nav />
+                    <Content />
+                    
+                </div>
+            </div>
+        </section>
      );
    }
  }
---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="col-md-10">
+                <h1>Content component is working</h1>
+            </div>
+        );
+    }
+}
+
+export default Content;
