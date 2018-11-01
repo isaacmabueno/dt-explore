8823015df34255510bfd0132efe61311dc012de3
isaacmabueno <isaacmabueno@gmail.com>
2018-10-31 21:03:17 -0500

    updating nav component with images and url

--- src/Nav.js
+++ src/Nav.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 import './App.css'
 
-const sidebarArray = ([
-    'The Digital Marketers Playbook for Voice Analytics',
-    'Optimize Google Ads, LinkedIn & Facebook ROI with Voice Analytics',
-    '6 AI-Based Call Reports That Will Transform Your Digital Marketing ROI',
-    'AI-Powered Conversation Analytics',
-    'Target Caller-Ready Audiences',
-    'Request A Demo'
-]);
+const sidebarArray = [
+    {icon:'http://www.dialogtech.com/wp-content/themes/ifbyphone/theme/images/media-center/icons/white/blog.svg', title: 'The Digital Marketers Playbook for Voice Analytics', url: 'https://www.dialogtech.com/customer-success'},
+    {icon:'http://www.dialogtech.com/wp-content/themes/ifbyphone/theme/images/media-center/icons/white/blog.svg', title: 'Optimize Google Ads, LinkedIn & Facebook ROI with Voice Analytics', url: 'https://www.dialogtech.com'},
+    {icon:'http://www.dialogtech.com/wp-content/themes/ifbyphone/theme/images/media-center/icons/white/blog.svg', title: '6 AI-Based Call Reports That Will Transform Your Digital Marketing ROI', url: 'https://www.dialogtech.com/careers'},
+    {icon:'http://www.dialogtech.com/wp-content/themes/ifbyphone/theme/images/media-center/icons/white/blog.svg', title: 'AI-Powered Conversation Analytics', url: 'https://www.dialogtech.com/press'},
+    {icon:'http://www.dialogtech.com/wp-content/themes/ifbyphone/theme/images/media-center/icons/white/blog.svg', title: 'Target Caller-Ready Audiences', url: 'https://www.dialogtech.com/about'},
+    {icon:'http://www.dialogtech.com/wp-content/themes/ifbyphone/theme/images/media-center/icons/white/blog.svg', title: 'Request A Demo', url: 'https://www.dialogtech.com/request-demo'}
+  ];
 const listItem = sidebarArray.map((item) =>
-    <li className="suggested-list">{item}</li>
+    <div className="suggested-list list-content"><img src={item.icon}></img><a href={item.url}>{item.title}</a></div>
 );
-
 const logo = <img src ="https://277y2d10y5kk16f38t1xjp9f-wpengine.netdna-ssl.com/wp-content/themes/ifbyphone/theme/images/logos/logo-dt-main.svg"></img>
 class Nav extends Component {
     render() {
         return (
             <nav class="col-md-2 nopad">
-            <div className="logo-container">
-                {logo}
-            </div>
-                <ul>
-                {listItem}
-                </ul>
+                <div className="logo-container">
+                    {logo}
+                </div>
+                <div>
+                    <ul className="list">
+                        <li className="list-item">
+                            {listItem}
+                        </li>
+                    </ul>
+                </div>
             </nav>
         );
     }
